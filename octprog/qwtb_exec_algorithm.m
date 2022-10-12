0112b1ee696b861c244c4c11783363bf265e0ec7
KaeroDot <KaeroDot@contact.me.on.git.hub>
2022-10-12 16:27:38 +0200

    add: octprog: reading phase info from all_phase_algs

--- octprog/qwtb_exec_algorithm.m
+++ octprog/qwtb_exec_algorithm.m
@@ -283,6 +283,8 @@ function [] = qwtb_exec_algorithm(meas_file, calc_unc, is_last_avg, avg_id, grou
     phases = unique(data.corr.phase_idx);
     
     % build channel-quantities names ('u1','i1','u2','i2',...)
+    % (For algorithms supporting all phases at once, the value of channels will
+    % be rebuilt after getting results of the algorithm!)
     channels = {}; 
     uis = {'u';'i'};
     for c = 1:numel(data.corr.tran)
@@ -624,10 +626,6 @@ function [] = qwtb_exec_algorithm(meas_file, calc_unc, is_last_avg, avg_id, grou
             end % for each phase (transducer)
         else
             % algorithm supports all phases at once, just put everything into cells
-            % make sampled data:
-            % store list of channels to results file         
-            rinf = infosettextmatrix(rinf, 'list', channels);
-            infosave(rinf, result_path);
             % copy user parameters to the QWTB input quantities:
             di = inputs;
             % --- for max of transducers/digitizer channels
@@ -706,17 +704,42 @@ function [] = qwtb_exec_algorithm(meas_file, calc_unc, is_last_avg, avg_id, grou
             di = cells_to_matrices(diC, alginfo);
             % execute algorithm (call qwtb):
             dout = qwtb(alg_id, di, calcset);
-            % discard uncertainties if unc. disabled:
-            if strcmpi(unc_mode,'none')
-                dout = qwtb_rem_unc(dout);    
-            end
-            % store current channel phase setup info (index; U, I tag)
-            phase_info.index = data.corr.phase_idx(p);
-            phase_info.tags = tags;
-            phase_info.section = list{p};
-            % store results to the result file
-            qwtb_store_results(result_path, dout, alginfo, phase_info);
-        else
+            % convert back from n+1 dimensional matrices to phases, if needed:
+            doC = matrices_to_cells(dout);
+
+            % get phase info from the algorithm outputs, if available:
+            phase_info = cell();
+            channels = cell();
+            for p = 1:numel(doC)
+                % discard uncertainties if unc. disabled:
+                if strcmpi(unc_mode,'none')
+                    doC{p} = qwtb_rem_unc(doC{p});    
+                end
+                % algorithm supplied phase informations
+                if isfield(dout, 'phase_info_index') & isfield(dout, 'phase_info_tags') & isfield(dout, 'phase_info_section')
+                    phase_info{p}.index = doC{p}.phase_info_index.v;
+                    phase_info{p}.tags = {doC{p}.phase_info_tags.v};
+                    phase_info{p}.section = doC{p}.phase_info_section.v;
+                    channels{p} = phase_info{p}.section;
+                else
+                    % create general phase informations
+                    phase_info{p}.index = p;
+                    phase_info{p}.tags = {num2str(p)};
+                    phase_info{p}.section = sprintf('L%d', p);
+                    channels{p} = phase_info{p}.section;
+                end
+            end % for p = 1:numel(diC)
+
+            % store list of channels to results file         
+            rinf = infosettextmatrix(rinf, 'list', channels);
+            infosave(rinf, result_path);
+
+            % save rest of results:
+            for p = 1:numel(doC)
+                % store results to the result file
+                qwtb_store_results(result_path, doC{p}, alginfo, phase_info{p});
+            end % for p
+        else % no support_all_phase
             % run qwtb algorithm one phase/transducer by another:
             for p = 1:numel(diC)
                     % execute algorithm (call qwtb):
@@ -959,102 +982,6 @@ function [din] = qwtb_rem_unc(din)
     end    
 end
 
-function [din] = cells_to_matrices(diC, alginfo)
-% Reorder data from the Cell diC of size N with structures with matrices to a
-% Strucure with matrices of one dimension more. The added dimension will have
-% size N.
-% This is done with respect to the qwtb and TWM quantities, so parameter
-% dimensions are not increased.
-% If matrices diC{i}.Q.F and diC{j}.Q.F are not of same dimensions, they are
-% padded by NaNs to the size of larger one.
-% Structures in cells must have the same fields to whole depth.
-
-% List of possible quantity fields in QWTB:
-QWTBf = {'v', 'u', 'd', 'c', 'r'};
-
-% list of quantities of the algorithm:
-Qs = fieldnames(diC{1});
-
-% list of quantities that are parameters:
-ids = logical([[alginfo.inputs].parameter]);
-Qparams = {[alginfo.inputs].name}(ids);
-
-% Go one quantity Q after another and concatenate quantity matrices from cells
-% to one matrix with dimension increased by 1:
-for q = 1:numel(Qs)
-    Q = Qs{q};
-    if any(strcmp(Q, Qparams))
-        % If Q is parameter type, concatenate all values into a cells, because
-        % parameter can take any values inside.
-        for c = 1:numel(diC)
-            din.(Q).v{c} = diC{c}.(Q).v;
-        end
-    else
-        % Q is not of parameter type, concatenate values together.
-        % For every field of quantity Q:
-        for f = 1:numel(QWTBf)
-            F = QWTBf{f};
-            Fc = {};
-            % Sizes of matrices in Q.F for every cell:
-            Fsizes = {};
-            % Dimensions of matrices in Q.F for every cell:
-            Fdims = [];
-            % Get sizes and dimensions of matrices in Q.F before concatenating
-            % so we will know how to padd it if needed:
-            for c = 1:numel(diC)
-                if isfield(diC{c}.(Q), F);
-                    Fc{end+1} = diC{c}.(Q).(F);
-                else
-                    Fc{end+1} = [];
-                end % isfield F
-                Fsizes{end+1} = size(Fc{end});
-                Fdims(end+1) = ndims(Fc{end});
-            end
-            % The maximal dimension is:
-            max_dim = max(Fdims);
-            % Check if all matrices got the same number of dimensions:
-            if all(Fdims == Fdims(1))
-                % Number of dimensions are same for all matrices in all cells
-                % for actual Q.F.
-                Fsizesmat = cat(1, Fsizes{:});
-                % Now check if all matrices are of same sizes:
-                if all(Fsizesmat(1, :) == Fsizesmat(:, :), 1)
-                    % Concatenate matrices into matrix of N+1 dimension
-                    % directly, because all matrices got same sizes. No padding
-                    % is needed:
-                    din.(Q).(F) = cat(max_dim + 1, Fc{:});
-                else
-                    % Matrices are not of the same size and must be paded by
-                    % NaN to concatenate.
-                    % Create matrix of NaNs of dimensions equal to maximal
-                    % dimensions:
-                    Fsizesmatmax = max(Fsizesmat, [], 1);
-                    Osizes = [max(Fsizesmat, [], 1) numel(diC)];
-                    O = NaN + zeros(Osizes);
-                    % Make a structure for subscript assignement:
-                    S.subs = cell(numel(Fsizesmatmax) + 1, 1);
-                    S.type = '()';
-                    % Through all cells assign matrices to O:
-                    for c = 1:numel(Fc)
-                        for d = 1:numel(Fsizes{c})
-                            S.subs{d} = 1:Fsizes{c}(d);
-                        end
-                        S.subs{end} = c;
-                        O = subsasgn(O, S, Fc{c});
-                    end
-                    din.(Q).(F) = O;
-                end % if all(Fsizesmat(1, :) == Fsizesmat(:, :), 1)
-            else
-                % Number of dimensions is different for matrices in cells for
-                % actual Q.F.
-                error('Concatenating matrices with different number of dimensions. This should not happen. Internal error.')
-            end % if all(Fdims == Fdims(1))
-        end % for f = 1:numel(QWTBf)
-    end % if any(strcmp(Q, Qparams))
-end % for q = 1:numel(Qs)
-
-end % function [din] = cells_to_matrices(diC, alginfo)
-
 function di = add_missing_fields(di, template)
     templateQns = fieldnames(template);
     diQns = fieldnames(di);
@@ -1068,3 +995,4 @@ function di = add_missing_fields(di, template)
         end % for f = 1:numel(Fns)
     end % for q = 1:numel(missing)
 end % function di = add_missing_fields(di, template)
+
